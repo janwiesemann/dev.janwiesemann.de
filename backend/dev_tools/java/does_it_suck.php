c288cf2c97a0e4ad3351ae309be3bf7b909691c0
Jan Wiesemann <kontakt@janwiesemann.de>
2022-10-27 14:19:17 +0200

    java/does_it_suck tool

---
+++ backend/dev_tools/java/does_it_suck.php
@@ -0,0 +1,17 @@
+﻿<?php
+require_once(__DIR__ . '/../../SectionTool.php');
+
+class does_it_suck extends SectionTool
+{
+    public function __construct()
+    {
+        parent::__construct('Java', description: 'This will finally answer one of the most asked questions in the entire universe.');
+    }
+
+    function IncludeSectionBody(): void
+    {
+?>
+        <h1>Yes!</h1>
+<?php
+    }
+}
